c9c47747de446929393838b0b5b7748833ee7c38
Ubuntu <ubuntu@ip-172-31-7-225.us-east-2.compute.internal>
2021-07-20 22:13:43 +0000

    First commit

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,4 +4,31 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
+
+  get name() {
+    return (this._name);
+  }
+
+  get length() {
+    return (this._length);
+  }
+
+  get students() {
+    return (this._name);
+  }
+
+  set name(newName) {
+    if (typeof (newName) !== 'string') throw TypeError('Name must be a string');
+    this._name = newName;
+  }
+
+  set length(newLength) {
+    if (typeof (newLength) !== 'number') throw TypeError('Length must be a number');
+    this._length = newLength;
+  }
+
+  set students(newStudents) {
+    if (typeof (newStudents) !== 'string') throw TypeError('Students must be an array of strings');
+    this._students = newStudents;
+  }
 }
