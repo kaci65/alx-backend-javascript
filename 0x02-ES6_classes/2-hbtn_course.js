51179a9ef2701ac4bbce3c8b31be60b1de0d9b4d
Ubuntu <ubuntu@ip-172-31-7-225.us-east-2.compute.internal>
2021-07-20 22:41:19 +0000

    Edit: add type checking below constructor

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,13 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof (name) !== 'string') throw TypeError('Name must be a string');
+    if (typeof (length) !== 'number') throw TypeError('Length must be a number');
+    students.forEach((student) => {
+      if (typeof (student) !== 'string') {
+        throw TypeError('Students must be an array of strings');
+      }
+    });
+
     this._name = name;
     this._length = length;
     this._students = students;
