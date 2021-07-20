d5a5427aa99227e55b87de581f434f43510935a1
Ubuntu <ubuntu@ip-172-31-7-225.us-east-2.compute.internal>
2021-07-20 22:59:13 +0000

    Revert to previous naming in setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -14,33 +14,33 @@ export default class HolbertonCourse {
   }
 
   get name() {
-    return (this._name);
+    return this._name;
   }
 
   get length() {
-    return (this._length);
+    return this._length;
   }
 
   get students() {
-    return (this._name);
+    return this._name;
   }
 
-  set name(name) {
-    if (typeof (name) !== 'string') throw TypeError('Name must be a string');
-    this._name = name;
+  set name(newName) {
+    if (typeof (newName) !== 'string') throw TypeError('Name must be a string');
+    this._name = newName;
   }
 
-  set length(length) {
-    if (typeof (length) !== 'number') throw TypeError('Length must be a number');
-    this._length = length;
+  set length(newLength) {
+    if (typeof (newLength) !== 'number') throw TypeError('Length must be a number');
+    this._length = newLength;
   }
 
-  set students(students) {
-    students.forEach((student) => {
-      if (typeof (student) !== 'string') {
+  set students(newStudents) {
+    newStudents.forEach((newStudent) => {
+      if (typeof (newStudent) !== 'string') {
         throw TypeError('Students must be an array of strings');
       }
     });
-    this._students = students;
+    this._students = newStudents;
   }
 }
