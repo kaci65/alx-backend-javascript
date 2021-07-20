da3d62df3dadfcd6199b4908628383a7dad7fe87
Ubuntu <ubuntu@ip-172-31-7-225.us-east-2.compute.internal>
2021-07-20 21:25:54 +0000

    Fix file

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,8 @@
-import {ClassRoom} from './0-classroom'
+import ClassRoom from './0-classroom';
 
-
-export default initializeRooms() {
-  const arr = [];
-  for x 
+export default function initializeRooms() {
+  const obj1 = new ClassRoom(19);
+  const obj2 = new ClassRoom(20);
+  const obj3 = new ClassRoom(34);
+  return ([obj1, obj2, obj3]);
 }
