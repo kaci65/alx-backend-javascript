1084456b591ec5a5ec390012035f370df4e3b7bb
Ubuntu <ubuntu@ip-172-31-7-225.us-east-2.compute.internal>
2021-07-21 15:13:45 +0000

    Edit: remove indexOf()

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
 export default function getStudentsByLocation(students, city) {
-  return students.filter((student) => student.location.indexOf(city) !== -1);
+  return students.filter((student) => student.location === city);
 }
