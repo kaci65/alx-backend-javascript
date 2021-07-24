e4a95d02c4cb7fca0c7cbb6182fa5103e86dbdef
Ubuntu <ubuntu@ip-172-31-7-225.us-east-2.compute.internal>
2021-07-24 23:29:36 +0000

    First commit

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+const groceryMap = new Map([
+  ['Apples', 10],
+  ['Tomatoes', 10],
+  ['Pasta', 1],
+  ['Rice', 1],
+  ['Banana', 5],
+]);
+
+export default function groceriesList() {
+  return groceryMap;
+}
