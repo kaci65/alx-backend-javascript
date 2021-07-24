479197b6895bf03555a3a621d1c72fdbd6fd6830
Ubuntu <ubuntu@ip-172-31-7-225.us-east-2.compute.internal>
2021-07-24 20:03:03 +0000

    Edit: revert to first state

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,21 @@
+const studentsList = [
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
+];
+
 export default function getListStudents() {
-  const studentsList = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Colombia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
   return studentsList;
 }
